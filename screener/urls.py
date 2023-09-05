94b9181fef146c0a0356347a09c9bed04a925df3
Fr3sto <fr3sto@yandex.ru>
2023-09-05 17:35:26 +0300

    delete all

--- screener/urls.py
+++
@@ -1,9 +0,0 @@
-from django.urls import path
-from . import views
-
-app_name = 'screener'
-urlpatterns = [
-    path('', views.index, name='index'),
-    path('<str:name>-<int:tf>', views.test_view, name='test_view'),
-    path('<str:name>', views.single_currency, name='single_currency')
-]
