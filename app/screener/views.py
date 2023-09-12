906cd5139558b76a863b35e45be72a17744a4d2b
Fr3sto <fr3sto@yandex.ru>
2023-09-12 12:59:19 +0300

    Correct Impulse Data

--- app/screener/views.py
+++ app/screener/views.py
@@ -5,16 +5,36 @@ from .models import CurrencyTable, Impulses, Currency, Candles
 from .db_request import getCandlesDF
 from .math_methods import getChartWithImpulse,impulse_long
 
+from datetime import datetime, timedelta
 
+from pytz import timezone
 
+import numpy as np
+
+tz=timezone('Europe/Moscow')
 def index(request):
     result = []
     currency = Currency.objects.all()
     for curr in currency:
+        countC1 = 0
+        countC5 = 0
+        countC15 = 0
         imp1 = Impulses.objects.get(symbol = curr, tf = 1, type = 'L')
+        if imp1.isOpen == 1:
+            dateNow = datetime.now(tz)
+            diffMin = dateNow - imp5.dateEnd + timedelta(hours=3)
+            countC1 = np.round(diffMin.total_seconds() / 60,0)
         imp5 = Impulses.objects.get(symbol = curr, tf = 5, type = 'L')
+        if imp5.isOpen == 1:
+            dateNow = datetime.now(tz)
+            diffMin = dateNow - imp5.dateEnd + timedelta(hours=3)
+            countC5 = np.round(diffMin.total_seconds() / 60 / 5,0)
         imp15 = Impulses.objects.get(symbol = curr, tf = 15, type = 'L')
-        result.append({'name' : curr.name, 'tf1' : imp1, 'tf5': imp5, 'tf15':imp15})
+        if imp15.isOpen == 1:
+            dateNow = datetime.now(tz)
+            diffMin = dateNow - imp5.dateEnd + timedelta(hours=3)
+            countC15 = np.round(diffMin.total_seconds() / 60 / 15,0)
+        result.append({'name' : curr.name, 'tf1' : imp1,'countC1':countC1, 'tf5': imp5,'countC5':countC5, 'tf15':imp15, 'countC15':countC15})
     return render(request, 'screener/currency_table.html', {'result':result})
 
 def single_currency(request, name):
